f8a01d0728678a55d4cf02f4705f1fb0e93a121e
Michael <82455991+mike-prog-lab@users.noreply.github.com>
2021-09-09 14:45:23 +0300

    Fixed ports mapping.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-docker rm -f prw-ws-server
+#docker rm -f prw-ws-server
 
 docker run \
     --name prw-ws-server \
-    --port 6001:6001 \
+    -p 6001:6001 \
     --env APP_KEY="$APP_KEY" \
     --env APP_ENV="$APP_ENV" \
     --env PUSHER_APP_ID="$PUSHER_APP_ID" \
@@ -11,5 +11,5 @@ docker run \
     --env PUSHER_APP_CLUSTER="$PUSHER_APP_CLUSTER" \
     --env MIX_PUSHER_APP_KEY="$MIX_PUSHER_APP_KEY" \
     --env MIX_PUSHER_APP_CLUSTER="$MIX_PUSHER_APP_CLUSTER" \
-    docker.pkg.github.com//prw-php-fpm:"$APP_VERSION"
+    docker.pkg.github.com/contact-funnels-ltd/prw-websocket-server/prw-ws-server:"$APP_VERSION"
 
